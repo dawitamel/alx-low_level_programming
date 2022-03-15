20d66163a8a2057efb3098e27ee982524cdd4086
dawit amel <dawit.amel@gmail.com>
2022-03-15 05:56:05 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ * main - Prints Holberton
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-
 	return (0);
 }
